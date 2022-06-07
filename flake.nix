309b5bbf7f7151952ec4f6823ae30022dd066521
SnO2WMaN <me@sno2wman.net>
2022-06-07 23:38:28 +0900

    naerskを使って統合

--- flake.nix
+++ flake.nix
@@ -3,34 +3,52 @@
     nixpkgs = {
       url = "github:nixos/nixpkgs/nixos-unstable";
     };
-    nci = {
-      url = "github:yusdacra/nix-cargo-integration";
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+    };
+    naersk = {
+      url = "github:nix-community/naersk";
       inputs.nixpkgs.follows = "nixpkgs";
+    };
+    devshell = {
+      url = "github:numtide/devshell";
     };
     flake-compat = {
       url = "github:edolstra/flake-compat";
       flake = false;
     };
   };
-  outputs = { self, nixpkgs, nci, ... } @ inputs:
-    inputs.nci.lib.makeOutputs {
-      root = ./.;
-      overrides = {
-        shell = common: prev: {
-          packages =
-            prev.packages
-            ++ (with common.pkgs; [
-              cargo-edit
-              cargo-make
-              cargo-watch
-              nixpkgs-fmt
-              rust-analyzer
-              taplo-cli
-              treefmt
-            ]);
-          commands = prev.commands;
-          env = prev.env;
+  outputs = { self, nixpkgs, flake-utils, naersk, devshell, ... } @ inputs:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ devshell.overlay ];
         };
-      };
-    };
+        naersk-lib = naersk.lib."${system}";
+      in
+      rec {
+        # `nix build`
+        packages.satysfi-formatter = naersk-lib.buildPackage {
+          pname = "satysfi-formatter";
+          root = ./.;
+        };
+        defaultPackage = packages.satysfi-formatter;
+
+        # `nix run`
+        apps.satysfi-formatter = flake-utils.lib.mkApp {
+          drv = packages.satysfi-formatter;
+        };
+        defaultApp = apps.satysfi-formatter;
+
+        # `nix develop`
+        devShell = pkgs.devshell.mkShell {
+          imports = [
+            (pkgs.devshell.importTOML ./devshell.toml)
+          ];
+          packages = [
+            packages.satysfi-formatter
+          ];
+        };
+      });
 }
